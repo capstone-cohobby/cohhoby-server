ec2a5a71580a8a7968c93cbec28193d016495f85
sunggyeong <surreal0122@naver.com>
2025-12-04 17:13:53 +0900

    fix: 프론트 주소로 수정

--- cohobby/src/main/java/com/backthree/cohobby/global/config/security/OAuth2LoginSuccessHandler.java
+++ cohobby/src/main/java/com/backthree/cohobby/global/config/security/OAuth2LoginSuccessHandler.java
@@ -9,6 +9,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -79,12 +80,14 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         } else {
             // 기본값: 프론트엔드 로그인 페이지로 리다이렉트 (동적 URL 사용)
             // 서버 포트를 프론트엔드 포트로 변경
-            String frontendUrl = "https://cohobby-git-dev-sunggyeongs-projects.vercel.app?_vercel_share=CUntubjwhufBXSZH6L1GLnJE121IrOdg";
-            if (!frontendUrl.contains(":3000") && !frontendUrl.contains(":80") && !frontendUrl.contains(":443")) {
-                // 포트가 없거나 다른 경우 명시적으로 3000 포트 추가
-                frontendUrl = scheme + "://" + serverName;
-            }
-            targetUrl = UriComponentsBuilder.fromUriString(frontendUrl + "/login")
+            @Value("${app.frontend.url}")
+            private String frontendUrl; // yml에서 값 주입
+
+            // ... (중략) ...
+
+            // 리다이렉트 로직
+            targetUrl = UriComponentsBuilder.fromUriString(frontendUrl)
+                    .path("/login")
                     .queryParam("accessToken", tokenDTO.getAccessToken())
                     .queryParam("refreshToken", tokenDTO.getRefreshToken())
                     .build().toUriString();
